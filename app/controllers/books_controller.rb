905cbfc8657f16193f209f4ca1a081ad1d9db573
EmiHasegawa0125 <s1f101902499@iniad.org>
2021-06-16 19:51:14 +0000

    [Finish] 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,59 @@
 class BooksController < ApplicationController
   def top
   end
-  
+
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
-  def new
+  # def news
+  #   @book = Book.new
+  # end
+
+  def create
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    if book.update(book_params)
+      flash[:notice] = "Book was successfully edited."
+      redirect_to book_path(book)
+    else
+      flash.now[:alert] = "some errors prohibited this book from being saved"
+      @book = Book.find(params[:id])
+      render :edit
+      # redirect_to edit_book_path(book)
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+      flash[:notice] = "Book was successfully destroyed."
+      redirect_to books_path
+    end
+  end
+
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
